db95ecb30780b0d28bbbc1ea432a8ca85bf81e75
truthmiles <truthmiles@msn.com>
2017-05-23 07:22:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,20 +7,11 @@ var bot = linebot({
   channelAccessToken: 'Zo1ykKjIvTmDj8BUw3h4pNKeuoou8mcxsTbHyQXcUv/01vx7jXR1WgM1MdSWWx2/yOxhwIhWJqm6wiSUcbrefkEdV3t/Tra+4x2Wxlxowaiq/4DH9xxDtW7KaiNs59nz1RHntuAt5cxKagSNVgEqCAdB04t89/1O/w1cDnyilFU='
 });
 
-bot.on('message', function(event) {
-  //console.log(event); //把收到訊息的 event 印出來看看
-    if (event.message.type = 'text') {
-    var msg = event.message.text;
-    event.reply(msg).then(function(data) {
-      // success 
-      console.log(msg);
-    }).catch(function(error) {
-      // error 
-      console.log('error');
-    });
-  }
-});
+var timer;
+var pm = [];
+_getJSON();
 
+_bot();
 const app = express();
 const linebotParser = bot.parser();
 app.post('/', linebotParser);
@@ -30,3 +21,45 @@ var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
   console.log("App now running on port", port);
 });
+
+function _bot() {
+  bot.on('message', function(event) {
+    if (event.message.type == 'text') {
+      var msg = event.message.text;
+      var replyMsg = '';
+      if (msg.indexOf('PM2.5') != -1) {
+        pm.forEach(function(e, i) {
+          if (msg.indexOf(e[0]) != -1) {
+            replyMsg = e[0] + '的 PM2.5 數值為 ' + e[1];
+          }
+        });
+        if (replyMsg == '') {
+          replyMsg = '請輸入正確的地點';
+        }
+      }
+      if (replyMsg == '') {
+        replyMsg = '不知道「'+msg+'」是什麼意思 :p';
+      }
+
+      event.reply(replyMsg).then(function(data) {
+        console.log(replyMsg);
+      }).catch(function(error) {
+        console.log('error');
+      });
+    }
+  });
+
+}
+
+function _getJSON() {
+  clearTimeout(timer);
+  getJSON('http://opendata2.epa.gov.tw/AQX.json', function(error, response) {
+    response.forEach(function(e, i) {
+      pm[i] = [];
+      pm[i][0] = e.SiteName;
+      pm[i][1] = e['PM2.5'] * 1;
+      pm[i][2] = e.PM10 * 1;
+    });
+  });
+  timer = setInterval(_getJSON, 1800000); //每半小時抓取一次新資料
+}
